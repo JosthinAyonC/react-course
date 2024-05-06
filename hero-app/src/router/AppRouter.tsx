74fe908eca779c147a26c14e42265892e5865c7a
Chustin <josthinayon@gmail.com>
2024-05-06 10:00:50 -0500

    Routes divididos

--- hero-app/src/router/AppRouter.tsx
+++ hero-app/src/router/AppRouter.tsx
@@ -1,24 +1,16 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { DCPage } from '../heros/pages/DCPage';
-import { MarvelPage } from '../heros/pages/MarvelPage';
-import { LoginPage } from '../auth/pages/LoginPage';
-import { Navbar } from '../ui';
+import { Route, Routes } from 'react-router-dom';
+import { LoginPage } from '../auth';
+import { HerosRoutes } from '../heros';
 
 export const AppRouter = () => {
     return (
         <>
-            <Navbar />
             <Routes>
-                <Route path='marvel' element={<MarvelPage />}></Route>
-                <Route path='dc' element={<DCPage />}></Route>
-
                 <Route path='login' element={<LoginPage />}></Route>
 
-                {/* Redirecciona un path a otra ruta haciendo uso del navigate  */}
-                <Route path='/' element={<Navigate to="marvel" />}></Route>
-
                 {/* Cuando no se encuentra una ruta, debe haber una default  */}
-                <Route path='*' element={<MarvelPage />}></Route>
+                <Route path='/*' element={<HerosRoutes />}></Route>
+
             </Routes>
         </>
     )
